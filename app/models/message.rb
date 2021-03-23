68237088bcc550feda2ba882c8f200ae90544cf8
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-03-23 19:15:16 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  has_one_attached :image
 
   validates :content, presence: true
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
